37c42447df537e9512d7ae08efb756197a4d95b3
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-12 22:28:16 +0530

    [Akshay] Retrieve details between two dates

--- AddressBookSystem/DBOperations.cs
+++ AddressBookSystem/DBOperations.cs
@@ -158,5 +158,79 @@ namespace AddressBookSystem
                 }
             }
         }
+
+        /// <summary>
+        /// UC18
+        /// Retrieves the contact between two dates.
+        /// </summary>
+        /// <param name="date1">The date1.</param>
+        /// <param name="date2">The date2.</param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception"></exception>
+        public static List<Contact> RetrieveContactBetweenTwoDates(string date1, string date2)
+        {
+            SqlConnection sqlConnection = DBConnection.GetConnection();
+            List<Contact> contactList = new List<Contact>();
+            try
+            {
+                using (sqlConnection)
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("dbo.spGetDetailsBetweenTwoDates", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@date1", date1);
+                    sqlCommand.Parameters.AddWithValue("@date2", date2);
+                    SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+                    if (dataReader.HasRows == false)
+                    {
+                        return contactList;
+                    }
+                    else
+                    {
+                        while (dataReader.Read())
+                        {
+                            string firstName = dataReader["first_name"].ToString();
+                            string lastName = dataReader["last_name"].ToString();
+                            string address = dataReader["address"].ToString();
+                            string city = dataReader["city"].ToString();
+                            string state = dataReader["state"].ToString();
+                            string zip = dataReader["zip"].ToString();
+                            string phoneNumber = dataReader["phone_number"].ToString();
+                            string email = dataReader["email"].ToString();
+                            string addressBookName = dataReader["name"].ToString();
+                            string type = dataReader["type"].ToString();
+
+                            Contact contact = new Contact();
+                            contact.firstName = firstName;
+                            contact.lastName = lastName;
+                            contact.address = address;
+                            contact.city = city;
+                            contact.state = state;
+                            contact.zip = zip;
+                            contact.phoneNumber = phoneNumber;
+                            contact.email = email;
+                            contact.addressBookName = addressBookName;
+                            contact.contactType = type;
+
+                            contactList.Add(contact);
+                        }
+                        dataReader.Close();
+                        return contactList;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (sqlConnection.State == ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
     }
 }
