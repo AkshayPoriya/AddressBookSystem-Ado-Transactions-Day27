c394b6610abe8d6ab35c5f024c0a52c368853b8a
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-12 22:02:57 +0530

    [Akshay] Update Contact Address

---
+++ AddressBookSystem/stored Procedures/spUpdateContactAddress.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE spUpdateContactAddress
+	@firstName varchar(50),
+	@lastName varchar(50),
+	@address varchar(50)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			UPDATE contacts
+			SET address = @address
+			WHERE first_name = @firstName AND last_name = @lastName
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+	END CATCH
+END
--- AddressBookSystemMSTest/TestDBOperations.cs
+++ AddressBookSystemMSTest/TestDBOperations.cs
@@ -9,12 +9,20 @@ namespace AddressBookSystemMSTest
     [TestClass]
     public class TestDBOperations
     {
+        /// <summary>
+        /// UC17
+        /// Tests the update address.
+        /// </summary>
         [TestMethod]
-        public void TestGetAllDetails()
+        public void TestUpdateAddress()
         {
             //Arrange
             //Act
+            DBOperations.UpdateContactAddress("Ben", "Geller", "New York City");
+            string actual = DBOperations.GetAddress("Ben", "Geller");
+            string expected = "New York City";
             //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
